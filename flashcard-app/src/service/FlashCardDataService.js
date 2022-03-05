3a0294fc27c9bffe1a4fcd585bcdcf3c17dcbdd1
Kendra Chauntell <kendrachauntell@outlook.com>
2022-03-05 15:22:11 -0500

    made service folder to connect to the back end

---
+++ flashcard-app/src/service/FlashCardDataService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+class FlashCardDataService {
+
+  retrieveAllFlashCards () {
+    return axios.get(`http://localhost:8080/flashcards`);
+  }
+  retrieveFlashCard(id) {
+    return axios.get(`http://localhost:8080/flashcards/${id}`)
+  }
+  addFlashCard(flashcard) {
+    return axios.post(`http://localhost:8080/flashcards`, flashcard)
+  }
+  updateFlashCard(flashcard) {
+    return axios.put(`http://localhost:8080/flashcards`, flashcard)
+  }
+  deleteFlashCard(id) {
+    return axios.delete(`http://localhost:8080/flashcards/${id}`)
+  }
+}
---
+++ flashcard-app/src/service/UserDataService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+class UserDataService {
+  retrieveAllUsers() {
+    return axios.get(`http://localhost:8080/users`);
+  }
+
+  retrieveUser(id) {
+    return axios.get(`http://localhost:8080/users/${id}`);
+  }
+
+  addUser(user) {
+    return axios.post(`http://localhost:8080/users`, user);
+  }
+
+  updateUser(user) {
+    return axios.put(`http://localhost:8080/users`, user);
+  }
+
+  deleteUser(id) {
+    return axios.delete(`http://localhost:8080/users/${id}`)
+  }
+}
+
+export default new UserDataService;
