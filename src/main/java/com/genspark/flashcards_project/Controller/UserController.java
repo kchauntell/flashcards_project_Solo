08cd7a7880ce6aa4fcc6ae6fedc8b6f7887d9dde
Kendra Chauntell <kendrachauntell@outlook.com>
2022-03-11 10:14:38 -0500

    got the code in, need to test it for logging in and logging out

--- src/main/java/com/genspark/flashcards_project/Controller/UserController.java
+++ src/main/java/com/genspark/flashcards_project/Controller/UserController.java
@@ -1,12 +1,15 @@
 package com.genspark.flashcards_project.Controller;
 
+import com.genspark.flashcards_project.DAO.UserRepository;
 import com.genspark.flashcards_project.Entity.User;
+import com.genspark.flashcards_project.Entity.Status;
 import com.genspark.flashcards_project.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = { "http://localhost:3000" })
@@ -14,6 +17,9 @@ import java.util.Optional;
 public class UserController {
   @Autowired
   public UserService userService;
+
+  @Autowired
+  UserRepository userRepository;
 
   @GetMapping("/users")
   public List<User> getAllUsers() {
@@ -39,5 +45,50 @@ public class UserController {
   public String deleteUser(@PathVariable Long id) {
     this.userService.deleteUser(id);
     return "Successfully Deleted User with ID: " + id;
+  }
+
+  //Register the user
+  @PostMapping("/users/register")
+  public Status registerUser(@Valid @RequestBody User newUser) {
+    List<User> users = userRepository.findAll();
+
+    for (User user : users) {
+      if(user.equals(newUser)) {
+        System.out.println("USER ALREADY EXISTS!");
+        return Status.USER_ALREADY_EXISTS;
+      }
+    }
+    userRepository.save(newUser);
+    return Status.SUCCESS;
+  }
+
+  // Setting up for Login
+  @PostMapping("/users/login")
+  public Status loginUser(@Valid @RequestBody User user) {
+    List<User> users = userRepository.findAll();
+
+    for(User other : users) {
+      if(other.equals(user)) {
+        user.setIsLoggedIn(true);
+        userRepository.save(user);
+        return Status.SUCCESS;
+      }
+    }
+    return Status.FAILURE;
+  }
+
+  //Logout user
+  @PostMapping("users/logout")
+  public Status logUserOut(@Valid @RequestBody User user) {
+    List<User> users = userRepository.findAll();
+
+    for(User other : users) {
+      if(other.equals(user)) {
+        user.setIsLoggedIn(false);
+        userRepository.save(user);
+        return Status.SUCCESS;
+      }
+    }
+    return Status.FAILURE;
   }
 }
---
+++ src/main/java/com/genspark/flashcards_project/Entity/Status.java
@@ -0,0 +1,7 @@
+package com.genspark.flashcards_project.Entity;
+
+public enum Status {
+  SUCCESS,
+  USER_ALREADY_EXISTS,
+  FAILURE
+}
